63c8e06d8a569aebf3d83cafdc2bab1e9a22fd0a
Sharath Kumar <sharathkr@protonmail.com>
2024-01-06 17:19:45 +0530

    feat: add sidebar component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,11 @@
+import Sidebar from '../components/Sidebar'
+
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  )
 }
 
 export default Dashboard
